a481faf90dbed62a31d1280e18704646839ec6cf
banikathuria <79353266+banikathuria@users.noreply.github.com>
2022-10-23 12:57:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-function preload()
+
 function setup(){
 canvas= createCanvas(300,300)
+canvas.position(615,300)
+video=createCapture(VIDEO)
+video.hide()
+classifier=ml5.imageClassifier("MobileNet",ModelLoaded)
 }
-function draw()
+function draw(){
+    image(video,0,0,300,300)
+    classifier.classify(video,gotResult)
+  
+}
 
-
+function ModelLoaded(){
+    console.log("Model has Loaded")
+}
+function gotResult(error,result){
+if (error){
+    console.log(error)
+    
+}
+else{
+    console.log(result)
+    document.getElementById("object").innerHTML=result[0].label
+    document.getElementById("accuracy").innerHTML=(result[0].confidence*100).toFixed(2)
+}
+}
 
